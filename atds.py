5f97089863db1f6a9d68e5396ca360d2a4c2120a
Jack Naliboff <jnaliboff25@students.polytechnic.org>
2024-03-01 09:43:39 -0800

    Added Node class

--- atds.py
+++ atds.py
@@ -78,3 +78,20 @@ class Deque(object):
 
     def is_empty(self):
         return len(self.deque) == 0
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def set_data(self, data):
+        self.data = data
+
+    def get_data(self):
+        return self.data
+
+    def set_next(self, next):
+        self.next = next
+
+    def get_next(self):
+        return self.next
